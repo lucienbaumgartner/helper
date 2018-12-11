1ebf51d9cea0ee2cb113a4f37e9edeaac61c0a6e
lucienbaumgartner <luciengeorges.baumgartner@uzh.ch>
2018-12-11 21:35:49 +0100

    My bad

--- python/selenium-hacks/scraper.py
+++ python/selenium-hacks/scraper.py
@@ -9,28 +9,25 @@ import pandas as pd
 import re
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('--headless')
 
-df = pd.read_csv('/Users/lucienbaumgartner/share/dev-temp/Link_List_NR_SR.csv')
-df = df[1:100]
-print(df)
+df = pd.read_csv('/path/to/csv')
 
 def scrape(page):
-    driver =  webdriver.Chrome(executable_path='/Users/lucienbaumgartner/Selenium/chromedriver', options = chrome_options)
+    driver =  webdriver.Chrome(executable_path='/path/to/chromedriver', options = chrome_options)
     try:
         driver.get(page)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="wsCouncilorGeneralInfo"]/div/div[1]/div[2]/h2')))
-        elem = driver.find_element_by_xpath('//*[@id="wsCouncilorGeneralInfo"]/div/div[1]/div[2]/h2')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, 'some-xpath')))
+        elem = driver.find_element_by_xpath('some-xpath')
         txt = elem.get_attribute('innerHTML').encode('utf-8').strip()
         if not isinstance(txt, str): txt = str(txt)
-        txt = re.sub('\s{2,}', '', txt)
-        print(txt)
+        txt = re.sub('\s{2,}', '', txt) # get rid of excess whitespace
         return txt
     except TimeoutException:
-        print "Loading took too much time!"
+        print('Loading took too much time!')
     finally:
         driver.close()
 
-df['name'] = [scrape(str(row)) for row in df['Links']]
-print(df)
-df.to_csv('/Users/lucienbaumgartner/share/dev-temp/pyscraper.csv')
+df['new-column'] = [scrape(str(row)) for row in df['column-name-links']]
+
+df.to_csv('/path/to/wd')
